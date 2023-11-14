34950fd397d8eab51ecafdeec90bfe568ae88a18
furkandlkdr <115067547+furkandlkdr@users.noreply.github.com>
2023-11-14 18:23:27 +0300

    Added temp improvements

---
+++ OgrenciNotTakipUygulaması/Insan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciNotTakipUygulaması
+{
+    internal class Insan
+    {
+        public string ad {  get; set; }
+        public string soyad {  get; set; }
+        public Insan() { }
+    }
+}
---
+++ OgrenciNotTakipUygulaması/Ogrenci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciNotTakipUygulaması
+{
+    internal class Ogrenci
+    {
+        public Insan insanOzellikleri = new Insan();
+        public int numara {  get; set; }
+        public double[] notlar {  get; set; }
+    }
+}
